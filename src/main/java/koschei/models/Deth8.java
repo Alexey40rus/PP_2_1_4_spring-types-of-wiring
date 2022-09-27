46fed8f0300cb5a2d73f5fe1576da47fb74be42e
Alexey40rus <mareich600@gmail.com>
2022-09-27 15:50:07 +0300

    1 версия

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -1,4 +1,12 @@
 package koschei.models;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Deth8 {
+    @Override
+    public String toString() {
+        return "Сказке конец";
+    }
 }
